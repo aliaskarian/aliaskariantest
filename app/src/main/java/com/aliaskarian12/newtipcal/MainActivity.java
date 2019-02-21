51dcfbd6b1df99ced2be7600a475a76ad4b4f8d7
aliAskarian <ali.askarian1992@gmail.com>
2019-02-21 13:13:26 +0330

    Initial commit

---
+++ app/src/main/java/com/aliaskarian12/newtipcal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aliaskarian12.newtipcal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
